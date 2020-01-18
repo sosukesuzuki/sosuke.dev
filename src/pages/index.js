33851e0a6df87930d8fe325367b03e0fd082352e
Sosuke Suzuki <aosukeke@gmail.com>
2020-01-19 07:15:33 +0900

    Add Profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,24 @@ import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import { graphql } from 'gatsby';
 import Anchor from '../components/Anchor';
+import logo from '../images/logo.jpeg';
 
+const Profile = styled.div`
+    display: flex;
+    margin-top: 10px;
+    margin-bottom: 30px;
+    h2,
+    p {
+        margin: 0;
+        font-size: 15px;
+    }
+    img {
+        width: 45px;
+        height: 45px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+`;
 const BlogPostItem = styled.h3`
     font-size: 15px;
     font-weight: normal;
@@ -16,6 +33,18 @@ export default function Index({ data }) {
     return (
         <Layout>
             <SEO />
+            <Profile>
+                <img src={logo} alt="sosukesuzuki" />
+                <div>
+                    <h2>鈴木 颯介</h2>
+                    <p>
+                        JavaScript と TypeScript と OSS が好きな大学生です。
+                        <Anchor gatsby href="/about">
+                            詳しくはこちら。
+                        </Anchor>
+                    </p>
+                </div>
+            </Profile>
             {posts.map(post => (
                 <BlogPostItem>
                     <span>{post.frontmatter.date}: </span>
