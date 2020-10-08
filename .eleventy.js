4675fb2423fa006c7062d1c3276d6fa7738706d2
sosukesuzuki <aosukeke@gmail.com>
2020-10-08 18:08:26 +0900

    linkify

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,12 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.setLibrary(
+    "md",
+    require("markdown-it")({
+      html: true,
+      breaks: true,
+      linkify: true,
+    })
+  );
 };
