869ef3c58bd7b847fe58c978cf26d446a5ce431d
sosukesuzuki <aosukeke@gmail.com>
2020-10-24 00:30:24 +0900

    OGP

--- _includes/layout.11ty.js
+++ _includes/layout.11ty.js
@@ -10,10 +10,29 @@ exports.render = function (data) {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
       <link rel="shortcut icon" href="/img/favicon.ico">
       <title>${data.title}</title>
+
       <style>${readCSSFiles(["/styles/styles.css"])}</style>
       ${data.css ? `<style>${data.css}</style>` : ""}
+      <meta name=author content="Sosuke Suzuki">
+
+      <meta name=description content="sosukesuzuki's personal website">
+
+      <meta name=twitter:card content=summary>
+      <meta name=twitter:site content=@__sosukesuzuki>
+      <meta name=twitter:url content=https://sosuke.dev>
+      <meta name=twitter:description content="sosukesuzuki's personal website">
+      <meta name=twitter:image content=https://sosuke.dev/img/logo.jpeg>
+
+      <meta property=og:type content=article>
+      <meta property=og:url content=https://sosuke.dev>
+      <meta property=og:title content=sosuke.dev>
+      <meta property=og:site_name content=sosuke.dev>
+      <meta property=og:description content="sosukesuzuki's personal website">
+      <meta property=og:image content=https://sosuke.dev/img/logo.jpeg>
+
     </head>
     <body>
       <header>
