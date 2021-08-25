e38b59581f3df36bf06c780786ae9dfe212a62d2
sosukesuzuki <aosukeke@gmail.com>
2021-08-25 15:39:02 +0900

    Issue と PR を分ける

--- github.11ty.js
+++ github.11ty.js
@@ -7,47 +7,63 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
+const QUERY = {
+  issue: "-user:sosukesuzuki author:sosukesuzuki is:public is:issue",
+  pr: "-user:sosukesuzuki author:sosukesuzuki is:public is:pr"
+};
+
+async function search(query) {
+  const { data } = await octokit.rest.search.issuesAndPullRequests({
+    q: query,
+    per_page: 25,
+    page: 1,
+  });
+  const items = data.items.map(({ html_url, title, number }) => {
+    const splitted = html_url.split("/");
+    const repo = `${splitted[3]}/${splitted[4]}`;
+    return {
+      repo,
+      number,
+      url: html_url,
+      title,
+    };
+  });
+  return items;
+}
+
+function renderItems(items) {
+  return `${items
+    .map(({ title, url, number, repo }) => {
+      return `<li>
+    <a href="${url}" target="_blank" rel="noopener">${repo}#${number}: ${title}</a>
+    </li>`;
+    })
+    .join("")}`;
+}
 module.exports = class {
   async data() {
-    const { data } = await octokit.rest.search.issuesAndPullRequests({
-      q:
-        "involves:sosukesuzuki -user:sosukesuzuki author:sosukesuzuki is:public ",
-      per_page: 100,
-      page: 1,
-    });
-    const items = data.items.map(({ html_url, title, number }) => {
-      const splitted = html_url.split("/");
-      const repo = `${splitted[3]}/${splitted[4]}`;
-      return {
-        repo,
-        number,
-        url: html_url,
-        title,
-      };
-    });
+    const issues = await search(QUERY.issue);
+    const pullRequests = await search(QUERY.pr);
     return {
       title: "sosukesuzuki's GitHub activities",
       layout: "layout.11ty.js",
       description:
-        "Latest 100 Issues and Pull Requests created by @sosukesuzuki",
-      items,
+        "Latest 25 Issues and 25 Pull Requests created by @sosukesuzuki",
+      issues,
+      pullRequests,
     };
   }
-  render({ items, description }) {
+  render({ description, issues, pullRequests }) {
     return `<div>
     <div>
-    <h2>My GitHub activities</h2>
     <p>${description}(last fetched: ${format(new Date(), "yyyy/MM/dd")})</p>
     </div>
     <div>
-    <ul>
-    ${items
-      .map(({ title, url, number, repo }) => {
-        return `<li>
-      <a href="${url}" target="_blank" rel="noopener">${repo}#${number}: ${title}</a>
-      </li>`;
-      })
-      .join("")}
+    <h2>Pull Requests</h2>
+    ${renderItems(pullRequests)}
+    <h2>Issues</h2>
+    <ul> 
+    ${renderItems(issues)}
     </ul>
     </div>
     </div>`;
