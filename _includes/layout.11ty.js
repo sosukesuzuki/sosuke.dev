1198b05e1d765f4f23d0edcedaec482ef33d213e
sosukesuzuki <aosukeke@gmail.com>
2021-12-30 01:48:44 +0900

    Add HTML comment

--- _includes/layout.11ty.js
+++ _includes/layout.11ty.js
@@ -14,52 +14,62 @@ exports.render = function (data) {
         .replace(/(<([^>]+)>)/gi, "")
         .slice(0, 120)
     : "sosukesuzuki's personal website";
-  return `<!doctype html>
-  <html lang="ja">
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  return /* HTML */ `<!DOCTYPE html>
+    <html lang="ja">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <link rel="shortcut icon" href="/img/favicon.ico">
-      <title>${data.title}</title>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <title>${data.title}</title>
 
-      <style>${readCSSFiles(["/styles/styles.css"])}</style>
-      ${data.css ? `<style>${data.css}</style>` : ""}
-      <meta name=author content="Sosuke Suzuki">
+        <style>
+          ${readCSSFiles(["/styles/styles.css"])}
+        </style>
+        ${data.css ? `<style>${data.css}</style>` : ""}
+        <meta name="author" content="Sosuke Suzuki" />
 
-      <meta name=description content="${description}">
+        <meta name="description" content="${description}" />
 
-      <meta name=twitter:card content=summary>
-      <meta name=twitter:site content=@__sosukesuzuki>
-      <meta name=twitter:url content=https://sosukesuzuki.dev>
-      <meta name=twitter:description content="${description}">
-      <meta name=twitter:image content=https://sosukesuzuki.dev/img/logo.jpeg>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@__sosukesuzuki" />
+        <meta name=twitter:url content=https://sosukesuzuki.dev>
+        <meta name="twitter:description" content="${description}" />
+        <meta name=twitter:image content=https://sosukesuzuki.dev/img/logo.jpeg>
 
-      <meta property=og:type content=article>
-      <meta property=og:url content=https://sosukesuzuki.dev>
-      <meta property=og:title content="${data.title}">
-      <meta property=og:site_name content=sosukesuzuki.dev>
-      <meta property=og:description content="${description}">
-      <meta property=og:image content=https://sosukesuzuki.dev/img/logo.jpeg>
-    </head>
-    <body>
-      <header>
-        <h1><a href="/">sosukesuzuki.dev</a></h1>
-        <div class="icons">
-          <a href="https://github.com/sosukesuzuki">
-            <img src="/img/github-icon.png" class="icon github-icon" width="30" height="30">
-          </a>
-          <a href="https://twitter.com/__sosukesuzuki">
-            <img src="/img/twitter-icon.png" class="icon twitter-icon" width="50" height="50">
-          </a>
-        </div>
-      </header>
-      <main>
-        ${data.content}
-      </main>
-      <footer>
-        <small>© 2019 Sosuke Suzuki</small>
-      </footer>
-    </body>
-  </html>`;
+        <meta property="og:type" content="article" />
+        <meta property=og:url content=https://sosukesuzuki.dev>
+        <meta property="og:title" content="${data.title}" />
+        <meta property="og:site_name" content="sosukesuzuki.dev" />
+        <meta property="og:description" content="${description}" />
+        <meta property=og:image content=https://sosukesuzuki.dev/img/logo.jpeg>
+      </head>
+      <body>
+        <header>
+          <h1><a href="/">sosukesuzuki.dev</a></h1>
+          <div class="icons">
+            <a href="https://github.com/sosukesuzuki">
+              <img
+                src="/img/github-icon.png"
+                class="icon github-icon"
+                width="30"
+                height="30"
+              />
+            </a>
+            <a href="https://twitter.com/__sosukesuzuki">
+              <img
+                src="/img/twitter-icon.png"
+                class="icon twitter-icon"
+                width="50"
+                height="50"
+              />
+            </a>
+          </div>
+        </header>
+        <main>${data.content}</main>
+        <footer>
+          <small>© 2019 Sosuke Suzuki</small>
+        </footer>
+      </body>
+    </html>`;
 };
